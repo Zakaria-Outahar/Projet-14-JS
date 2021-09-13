e1ef73a4fd157d5daa9b52ecf6e3bf711a422865
Zakaria Outahar <outaharz@gmail.com>
2021-09-13 11:34:20 +0200

    JavaScript : mise en mouvement des éléments

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const container = document.querySelector('.grille');
 let toutesLesDivs;
 let alienInvaders = [];
 let tireurPosition = 229;
+let direction = 1;
 
 function creationGrilleEtAliens(){
     let indexAttr = 0;
@@ -44,3 +45,61 @@ function creationGrilleEtAliens(){
 }
 
 creationGrilleEtAliens();
+
+function deplacerLeTireur(e){
+    toutesLesDivs[tireurPosition].classList.remove('tireur');
+    
+    if(e.keyCode === 37 && tireurPosition > 220){
+        tireurPosition -= 1;
+    }
+    if(e.keyCode === 39 && tireurPosition < 239){
+        tireurPosition += 1;
+    }
+
+    toutesLesDivs[tireurPosition].classList.add('tireur');
+}
+document.addEventListener('keydown', deplacerLeTireur);
+
+let descendreRight = true;
+let descendreLeft = true;
+
+function bougerLesAliens(){
+    for(let i = 0; i < alienInvaders.length; i++){
+        if(toutesLesDivs[alienInvaders[i]].getAttribute('data-right') === 'true'){
+           if(descendreRight){
+               direction = 20;
+               setTimeout(() => {
+                descendreRight = false;
+               }, 50);
+           } else if(!descendreRight){
+               direction = -1;
+           }
+           descendreLeft = true;
+        }
+
+        if(toutesLesDivs[alienInvaders[i]].getAttribute('data-left') === 'true'){
+            if(descendreLeft){
+                direction = 20;
+                setTimeout(() => {
+                 descendreLeft = false;
+                }, 50);
+            } else if(!descendreLeft){
+                direction = 1;
+            }
+            descendreRight = true;
+         }
+    }
+
+
+    for(let i = 0; i < alienInvaders.length; i++){
+        toutesLesDivs[alienInvaders[i]].classList.remove('alien');
+    }
+    for(let i = 0; i < alienInvaders.length; i++){
+        alienInvaders[i] += direction;
+    }
+    for(let i = 0; i < alienInvaders.length; i++){
+        toutesLesDivs[alienInvaders[i]].classList.add('alien');
+    }
+}
+
+let invaderId = setInterval(bougerLesAliens,500);
