c17df0cad6dcd06aa2a63faa8d48a339b48013c3
Zakaria Outahar <outaharz@gmail.com>
2021-09-13 10:52:22 +0200

    JavaScript : apparition statique du jeu en début de partie

--- app.js
+++ app.js
@@ -1,1 +1,46 @@
+const container = document.querySelector('.grille');
+let toutesLesDivs;
+let alienInvaders = [];
+let tireurPosition = 229;
 
+function creationGrilleEtAliens(){
+    let indexAttr = 0;
+    for(i = 0; i < 240; i++){
+        if(indexAttr === 0){
+            const bloc = document.createElement('div');
+            bloc.setAttribute('data-left','true');
+            container.appendChild(bloc);
+            indexAttr++;
+        } else if(indexAttr === 19){
+            const bloc = document.createElement('div');
+            bloc.setAttribute('data-right','true');
+            container.appendChild(bloc);
+            indexAttr=0;
+        } else{
+            const bloc = document.createElement('div');
+            container.appendChild(bloc);
+            indexAttr++;
+        }
+    }
+
+    for(i = 1; i < 53; i++){
+        if(i === 13){
+            i = 21;
+            alienInvaders.push(i);
+        } else if(i === 33){
+            i = 41;
+            alienInvaders.push(i);
+        } else{
+            alienInvaders.push(i);
+        }
+    }
+
+    toutesLesDivs = document.querySelectorAll('.grille div');
+    alienInvaders.forEach(invader => {
+        toutesLesDivs[invader].classList.add('alien');
+    })
+
+    toutesLesDivs[tireurPosition].classList.add('tireur');
+}
+
+creationGrilleEtAliens();
